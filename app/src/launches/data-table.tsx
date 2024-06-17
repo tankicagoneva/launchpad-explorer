d9e1d0b2cf838547155270067238f8eb91a546bc
tankicagoneva <tankicaspasev@gmail.com>
2024-06-17 20:36:59 +0100

    feat: improve pagination functionality

--- app/src/launches/data-table.tsx
+++ app/src/launches/data-table.tsx
@@ -19,6 +19,7 @@ import React from "react";
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -122,22 +123,51 @@ export function LaunchesDataTable<TData, TValue>({ columns, data }: DataTablePro
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-start space-x-2 py-4">
-        <Button variant={"outline"} size="sm" onClick={() => table.firstPage()} disabled={!table.getCanPreviousPage()}>
-          <ChevronsLeft size={18} strokeWidth={0.9} absoluteStrokeWidth />
-        </Button>
-        <Button variant={"outline"} size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
-          <ChevronLeft size={18} strokeWidth={0.9} absoluteStrokeWidth />
-        </Button>
-        <Button variant={"outline"} size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
-          <ChevronRight size={18} strokeWidth={0.9} absoluteStrokeWidth />
-        </Button>
-        <Button variant={"outline"} size="sm" onClick={() => table.lastPage()} disabled={!table.getCanNextPage()}>
-          <ChevronsRight size={18} strokeWidth={0.9} absoluteStrokeWidth />
-        </Button>
-      </div>
-      <div className="flex-1 text-sm text-muted-foreground">
-        {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} rows selected
+      <div className="flex-col flex items-start justify-between md:space-x-2 py-4 md:flex-row md:items-center ">
+        <div className="text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} rows selected
+        </div>
+        <div className="flex flex-col items-start pt-4 md:space-x-2 md:flex-row md:items-center md:pt-0">
+          <p className="text-sm font-medium">Rows per page</p>
+          <Select
+            value={`${table.getState().pagination.pageSize}`}
+            onValueChange={(value) => {
+              table.setPageSize(Number(value));
+            }}
+          >
+            <SelectTrigger className="h-8 w-[70px]">
+              <SelectValue placeholder={table.getState().pagination.pageSize} />
+            </SelectTrigger>
+            <SelectContent side="top">
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <SelectItem key={pageSize} value={`${pageSize}`}>
+                  {pageSize}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <div className="flex pt-4 md:items-center md:space-x-2 md:pt-0">
+            <div className="md:space-x-2 pr-4 md:px-4">
+              <p className="text-sm font-medium ">
+                Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+              </p>
+            </div>
+            <div>
+              <Button variant={"outline"} size="sm" onClick={() => table.firstPage()} disabled={!table.getCanPreviousPage()}>
+                <ChevronsLeft size={18} strokeWidth={0.9} absoluteStrokeWidth />
+              </Button>
+              <Button className="mx-2" variant={"outline"} size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+                <ChevronLeft size={18} strokeWidth={0.9} absoluteStrokeWidth />
+              </Button>
+              <Button className="mx-2" variant={"outline"} size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+                <ChevronRight size={18} strokeWidth={0.9} absoluteStrokeWidth />
+              </Button>
+              <Button variant={"outline"} size="sm" onClick={() => table.lastPage()} disabled={!table.getCanNextPage()}>
+                <ChevronsRight size={18} strokeWidth={0.9} absoluteStrokeWidth />
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
