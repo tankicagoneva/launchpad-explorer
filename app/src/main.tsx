ad71df8db000f69d6f025eace169bb47d118af05
tankicagoneva <tankicaspasev@gmail.com>
2024-07-28 17:26:23 +0100

    feat: introduce react router

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import ErrorPage from './error-page.tsx';
+import Launches from './launches/page.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+    {
+    path: "/launches",
+    element: <Launches />,
+    }
+  
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
