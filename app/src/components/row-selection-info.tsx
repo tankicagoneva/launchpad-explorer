9de8280c5b073a7fa6f81062396b6ce3b827c080
tankicagoneva <tankicaspasev@gmail.com>
2024-09-22 19:50:32 +0100

    feat: add test for row selection info

--- app/src/components/row-selection-info.tsx
+++ app/src/components/row-selection-info.tsx
@@ -1,6 +1,6 @@
 const RowSelectionInfo = ({ selectedRow, totalRows }: { selectedRow: number, totalRows: number }) => {
     return (
-      <div className="text-sm text-muted-foreground">
+      <div data-testid={'row-selection'}  className="text-sm text-muted-foreground">
         {selectedRow} of {totalRows} rows selected
       </div>
     )
---
+++ app/src/tests/row-selection-info.test.tsx
@@ -0,0 +1,35 @@
+import "@testing-library/jest-dom"
+import { test, expect } from "vitest"
+import { render, screen } from "@testing-library/react"
+import RowSelectionInfo from "@/components/row-selection-info";
+
+
+test("renders the row selection info", async ()  => {
+
+
+  const exampleProps = {
+    selectedRow: 1,
+    totalRows: 10,
+  }
+
+  render(<RowSelectionInfo {...exampleProps} />)
+
+  const rowSelectionInfo = await screen.findByTestId('row-selection');
+  
+  expect(rowSelectionInfo).toBeInTheDocument()
+
+  
+})
+
+test ("renders the correct row selection info", async () => {
+    const exampleProps = {
+        selectedRow: 1,
+        totalRows: 10,
+    }
+    
+    render(<RowSelectionInfo {...exampleProps} />)
+    
+    const rowSelectionInfo = await screen.findByTestId('row-selection');
+    
+    expect(rowSelectionInfo).toHaveTextContent("1 of 10 rows selected")
+})
